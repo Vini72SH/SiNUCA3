39b733059edf7c52e7964f203f3772d81972ba27
ferbcastro <fernandocuritiba3@gmail.com>
2025-02-14 01:10:38 -0300

    Initial version of memory trace generating tool

--- src/trace_generator/sinuca3_pintool.cpp
+++ src/trace_generator/sinuca3_pintool.cpp
@@ -1,14 +1,21 @@
 #include "pin.H"
-#include "/home/fbc04/Programas/SiNUCA3/src/utils/logging.hpp"
+#include "../utils/logging.hpp"
 #include "../trace_reader/orcs_trace_reader.hpp"
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <string>
+#ifdef debug
 #include <iostream> // REMOVE
+#endif
 
 #define BUFFER_SIZE 1048576
 #define BBL_ID_BYTE_SIZE 2
+#define MEMREAD_EA IARG_MEMORYREAD_EA
+#define MEMREAD_SIZE IARG_MEMORYREAD_SIZE
+#define MEMWRITE_EA IARG_MEMORYWRITE_EA
+#define MEMWRITE_SIZE IARG_MEMORYWRITE_SIZE
+#define MEMREAD2_EA IARG_MEMORYREAD2_EA
 
 // class definitions
 class buffer {
@@ -22,12 +29,12 @@ class buffer {
         this->minimumSpace = min;
     }
     ~buffer() {};
-    void loadBufToFile(FILE* file) {
+    inline void loadBufToFile(FILE* file) {
         size_t written = fwrite(this->store, 1, this->numUsedBytes, file);
         if (written < this->numUsedBytes) {SINUCA3_ERROR_PRINTF("Buffer error");}
         this->numUsedBytes=0;
     }
-    bool isBufFull() {
+    inline bool isBufFull() {
         return (BUFFER_SIZE - this->numUsedBytes < this->minimumSpace);    
     }
 };
@@ -51,7 +58,7 @@ VOID appendToDynamicTrace(UINT32 bblId) {
 
     std::memcpy(buf+*used, (void*)&bblId, BBL_ID_BYTE_SIZE);
     (*used)+=BBL_ID_BYTE_SIZE;
-    if(dynamicBuffer->isBufFull()) {
+    if(dynamicBuffer->isBufFull() == true) {
         dynamicBuffer->loadBufToFile(dynamicTrace);   
     }
 }
@@ -71,16 +78,14 @@ VOID stopInstrumentation(int bblCount) {
     fwrite((void*)&bblCount, 1, sizeof(bblCount), staticTrace);
     fseek(staticTrace, savedPosition, SEEK_SET);
 
-    char *buf=staticBuffer->store;
-    size_t *used=&staticBuffer->numUsedBytes; 
-    if (*used>0) {
-        fwrite(buf, 1, *used, staticTrace);
+    if (staticBuffer->numUsedBytes>0) {
+        staticBuffer->loadBufToFile(staticTrace);
     }
-
-    buf=dynamicBuffer->store;
-    used=&dynamicBuffer->numUsedBytes;
-    if (*used>0) {
-        fwrite(buf, 1, *used, dynamicTrace);
+    if (dynamicBuffer->numUsedBytes>0) {
+        dynamicBuffer->loadBufToFile(dynamicTrace);
+    }
+    if (memoryBuffer->numUsedBytes>0) {
+        memoryBuffer->loadBufToFile(memoryTrace);
     }
 }
 
@@ -142,6 +147,89 @@ VOID x86ToStaticBuf(INS* ins) {
         std::memcpy(buf+*used, (void*)&branchTypeByte, sizeof(branchTypeByte));
         (*used)+=sizeof(branchTypeByte);
     }
+
+    if (staticBuffer->isBufFull() == true) {
+        staticBuffer->loadBufToFile(staticTrace);
+    }
+}
+
+VOID oneReadingOrWriting(ADDRINT addr, INT32 size) {
+    char* buf = memoryBuffer->store;
+    size_t* used = &memoryBuffer->numUsedBytes;
+    std::memcpy(buf+*used, (void*)&addr, sizeof(addr));
+    (*used)+=sizeof(addr);
+    std::memcpy(buf+*used, (void*)&size, sizeof(size));
+    (*used)+=sizeof(size);
+    if (memoryBuffer->isBufFull() == true) {
+        memoryBuffer->loadBufToFile(memoryTrace);
+    } 
+}
+
+VOID noWritingTwoReadings(ADDRINT addr1, ADDRINT addr2, INT32 size) {
+    char* buf = memoryBuffer->store;
+    size_t* used = &memoryBuffer->numUsedBytes;
+    std::memcpy(buf+*used, (void*)&addr2, sizeof(addr2));
+    (*used)+=sizeof(addr2);
+    std::memcpy(buf+*used, (void*)&size, sizeof(size));
+    (*used)+=sizeof(size);
+    oneReadingOrWriting(addr2, size);
+}
+
+VOID oneReadingAndWriting(ADDRINT addr1, ADDRINT addr2, INT32 size1,
+                            INT32 size2) {
+    char* buf = memoryBuffer->store;
+    size_t* used = &memoryBuffer->numUsedBytes;
+    std::memcpy(buf+*used, (void*)&addr2, sizeof(addr2));
+    (*used)+=sizeof(addr2);
+    std::memcpy(buf+*used, (void*)&size2, sizeof(size2));
+    (*used)+=sizeof(size2);
+    oneReadingOrWriting(addr1, size1);
+}
+
+VOID twoReadingsOneWriting(ADDRINT addr1, ADDRINT addr2, ADDRINT addr3,
+                            INT32 size1, INT32 size2) {
+    char* buf = memoryBuffer->store;
+    size_t* used = &memoryBuffer->numUsedBytes;
+    std::memcpy(buf+*used, (void*)&addr3, sizeof(addr3));
+    (*used)+=sizeof(addr3);
+    std::memcpy(buf+*used, (void*)&size2, sizeof(size2));
+    (*used)+=sizeof(size2);
+    noWritingTwoReadings(addr1, addr2, size1);
+}
+
+// aaaaaah
+VOID instrumentMemIns(INS* ins) {
+    BOOL hasRead = INS_IsMemoryRead(*ins);
+    BOOL hasRead2 = INS_HasMemoryRead2(*ins);
+    BOOL hasWrite = INS_IsMemoryWrite(*ins);
+
+    if (!hasRead && !hasRead2 && !hasWrite) return;
+
+    if (!hasRead && !hasRead2 && hasWrite) {
+        INS_InsertCall(*ins, IPOINT_BEFORE, (AFUNPTR)oneReadingOrWriting,
+                        MEMWRITE_EA, MEMWRITE_SIZE, IARG_END);
+    } else if (!hasRead && hasRead2 && !hasWrite) {
+        INS_InsertCall(*ins, IPOINT_BEFORE, (AFUNPTR)oneReadingOrWriting,
+                        MEMREAD2_EA, MEMREAD_SIZE, IARG_END);
+    } else if (!hasRead && hasRead2 && hasWrite) {
+        INS_InsertCall(*ins, IPOINT_BEFORE, (AFUNPTR)oneReadingAndWriting,
+                        MEMREAD2_EA, MEMWRITE_EA, MEMREAD_SIZE, MEMWRITE_SIZE,
+                        IARG_END);
+    } else if (hasRead && !hasRead2 && !hasWrite) {
+        INS_InsertCall(*ins, IPOINT_BEFORE, (AFUNPTR)oneReadingOrWriting,
+                        MEMREAD_EA, MEMREAD_SIZE, IARG_END);   
+    } else if (hasRead && !hasRead2 && hasWrite) {
+        INS_InsertCall(*ins, IPOINT_BEFORE, (AFUNPTR)oneReadingAndWriting,
+                        MEMREAD_EA, MEMWRITE_EA, MEMREAD_SIZE, MEMWRITE_SIZE,
+                        IARG_END);   
+    } else if (hasRead && hasRead2 && !hasWrite) {
+        INS_InsertCall(*ins, IPOINT_BEFORE, (AFUNPTR)noWritingTwoReadings,
+                        MEMREAD_EA, MEMREAD2_EA, MEMREAD_SIZE, IARG_END); 
+    } else if (hasRead && hasRead2 && hasWrite) {
+        INS_InsertCall(*ins, IPOINT_BEFORE, (AFUNPTR)twoReadingsOneWriting,
+                        MEMREAD_EA, MEMREAD2_EA, MEMWRITE_EA, MEMREAD_SIZE,
+                        MEMWRITE_SIZE, IARG_END); 
+    }
 }
 
 VOID trace(TRACE trace, VOID *ptr) {
@@ -164,20 +252,8 @@ VOID trace(TRACE trace, VOID *ptr) {
         bblInit = (*usedStatic)++;
         for (INS ins = BBL_InsHead(bbl); INS_Valid(ins); ins = INS_Next(ins), numInstBbl++) {
             x86ToStaticBuf(&ins);
-            if (staticBuffer->isBufFull()) {
-                staticBuffer->loadBufToFile(staticTrace);
-            }
-
             if (!INS_IsStandardMemop(ins)) continue;
-            if (INS_IsMemoryRead(ins)) {
-
-            }
-            if (INS_HasMemoryRead2(ins)) {
-
-            }
-            if (INS_IsMemoryWrite(ins)) {
-
-            }
+            instrumentMemIns(&ins);
         }
         std::memcpy(buf+bblInit, (void*)&numInstBbl, 1);
         bblCount++;
@@ -194,15 +270,14 @@ VOID imageLoad(IMG img, VOID* ptr) {
         char fileName[64], subStr[32];
 
         size_t it = nameImg.find_last_of('/')+1;
-        strncpy(subStr, &nameImg.c_str()[it], nameImg.size()-it);
-        snprintf(fileName, sizeof(fileName), "static_%s.fft", subStr);
+        strncpy(subStr, &nameImg.c_str()[it], nameImg.size()-it+1);
+        snprintf(fileName, sizeof(fileName)-1, "static_%s.fft", subStr);
         staticTrace = fopen(fileName, "wb");
-        snprintf(fileName, sizeof(fileName), "dynamic_%s.fft", subStr);
+        snprintf(fileName, sizeof(fileName)-1, "dynamic_%s.fft", subStr);
         dynamicTrace = fopen(fileName, "wb");
-        snprintf(fileName, sizeof(fileName), "memory_%s.fft", subStr);
+        snprintf(fileName, sizeof(fileName)-1, "memory_%s.fft", subStr);
         memoryTrace = fopen(fileName, "wb");
     }
-
 
     for (SEC sec = IMG_SecHead(img); SEC_Valid(sec); sec = SEC_Next(sec)) {
         for (RTN rtn = SEC_RtnHead(sec); RTN_Valid(rtn); rtn = RTN_Next(rtn)) {
@@ -231,7 +306,7 @@ int main(int argc, char* argv[]) {
     
     staticBuffer = new buffer(64);
     dynamicBuffer = new buffer(BBL_ID_BYTE_SIZE);
-    memoryBuffer = new buffer(sizeof(long));
+    memoryBuffer = new buffer(3*sizeof(ADDRINT)+sizeof(INT32));
 
     IMG_AddInstrumentFunction(imageLoad, NULL);
     TRACE_AddInstrumentFunction(trace, NULL);
