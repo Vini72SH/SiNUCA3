0993adfe8a0b118b68ed90c76cae511d28aee98d
ferbcastro <fernandocuritiba3@gmail.com>
2025-05-01 21:31:32 -0300

    Introducing file handler specific to reader

---
+++ src/trace_reader/reader_file_handler.hpp
@@ -0,0 +1,24 @@
+#ifndef READERFILEHANDLER_HPP_
+#define READERFILEHANDLER_HPP_
+
+#include "../utils/file_handler.hpp"
+
+using namespace trace;
+
+namespace traceReader {
+
+  class StaticTraceFile : public TraceReader {
+
+  };
+
+  class DynamicTraceFile : public TraceReader {
+
+  };
+
+  class MemoryTraceFile : public TraceReader {
+
+  };
+
+} // namespace traceReader
+
+#endif
