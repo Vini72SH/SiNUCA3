2a630109c0addf2261acb384bd5af867a4c8acf4
bk304 <xnep1@hotmail.com>
2025-09-01 00:31:30 -0300

    Cache: Creating SimpleCache component

---
+++ src/std_components/memory/simple_cache.cpp
@@ -0,0 +1,64 @@
+//
+// Copyright (C) 2024  HiPES - Universidade Federal do Paraná
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+/**
+ * @file simple_cache.cpp
+ * @brief Implementation of the SimpleCache.
+ */
+
+ #include <sinuca3.hpp>
+ #include "simple_cache.hpp"
+
+int SimpleCache::FinishSetup(){
+    if(this->cache.FinishSetup())
+        return 1;
+
+    return 0;
+}
+
+int SimpleCache::SetConfigParameter(const char* parameter, ConfigValue value){
+    if(this->cache.SetConfigParameter(parameter, value))
+        return 1;
+
+    return 0;
+}
+
+void SimpleCache::Clock() {
+    SINUCA3_DEBUG_PRINTF("%p: SimpleCache Clock!\n", this);
+    long numberOfConnections = this->GetNumberOfConnections();
+    MemoryPacket packet;
+    for (long i = 0; i < numberOfConnections; ++i) {
+        if (this->ReceiveRequestFromConnection(i, &packet) == 0) {
+            ++this->numberOfRequests;
+
+            // We dont have (and dont need) data to send back, so a
+            // MemoryPacket is send back to to signal
+            // that the cache's operation has been completed.
+
+            // Read() returns true if it was hit.
+            if (this->cache.Read(packet)) {
+                this->SendResponseToConnection(i, &packet);
+            } else {
+                this->cache.Write(packet);
+            }
+        }
+    }
+}
+
+void SimpleCache::PrintStatistics(){
+
+}
---
+++ src/std_components/memory/simple_cache.hpp
@@ -0,0 +1,46 @@
+#ifndef SINUCA3_SIMPLE_CACHE_HPP_
+#define SINUCA3_SIMPLE_CACHE_HPP_
+
+//
+// Copyright (C) 2024  HiPES - Universidade Federal do Paraná
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+/**
+ * @file simple_cache.hpp
+ * @details Implementation of a SimpleCache without latency or coherence protocol.
+ */
+
+ #include <sinuca3.hpp>
+#include "engine/component.hpp"
+#include "engine/default_packets.hpp"
+#include "utils/cache/cache.hpp"
+
+class SimpleCache : public Component<MemoryPacket> {
+    public:
+        SimpleCache() : numberOfRequests(0) {};
+        virtual ~SimpleCache();
+
+        virtual int FinishSetup();
+        virtual int SetConfigParameter(const char* parameter, ConfigValue value);
+        virtual void Clock();
+        virtual void PrintStatistics();
+
+    private:
+    Cache cache;
+     unsigned int numberOfRequests;
+};
+
+ #endif
